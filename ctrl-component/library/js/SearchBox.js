8ed1b1bbc276b4a8af2f70822a1585240e87c0a2
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2018-06-27 21:51:48 +0400

    Контролируемые компоненты

--- ctrl-component/library/js/SearchBox.js
+++ ctrl-component/library/js/SearchBox.js
@@ -1,5 +1,16 @@
-const SearchBox = () => {
-    return (
-        <input type="text" placeholder="Поиск по названию или автору"/>
-    );
-};
+const SearchBox = (props) => {
+	
+	const filter = event => {
+		props.filterBooks(event.currentTarget.value);
+	}
+	
+	return (
+		<input 
+			type="text" 
+			placeholder="Поиск по названию или автору"
+			onChange={filter}
+			value={props.value}
+		/>
+	);
+}
+
--- ctrl-component/subscribeForm/js/SubscribeForm.js
+++ ctrl-component/subscribeForm/js/SubscribeForm.js
@@ -1,5 +1,53 @@
-const SubscribeForm = () => {
-    return (
-        <div>Поиск</div>
-    )
-};
+class SubscribeForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { 
+			email: '',
+			checkEmailValid: ' '
+		};
+		this.submit = this.submit.bind(this);
+		this.checkEmail = this.checkEmail.bind(this);
+	}
+
+	checkEmail(event) {
+		const isValidEmail = event.currentTarget.validity.valid ? 'is-valid' : 'is-error';
+		const checkLengthAndValidEmail =	!event.currentTarget.value.length ? ' ' : isValidEmail;
+		this.setState({
+			email: event.currentTarget.value,
+			checkEmailValid: checkLengthAndValidEmail
+		});
+	}
+
+	submit(event){
+		event.preventDefault();
+	}
+
+	render () {
+		return (
+			<div className="subscribe__form">
+				<form 
+					className={`form form--subscribe ${this.state.checkEmailValid}`}
+					onSubmit={this.submit}
+				>
+					<h4 className="form-title">Подписаться:</h4>
+					<div className="form-group">
+						<label htmlFor="input-email" className="sr-only">Email</label>
+						<input 
+							type="email" 
+							id="input-email" 
+							placeholder="Email" 
+							className="form-control"
+							value={this.state.email}
+							onChange={this.checkEmail} 
+							name="email"
+						/>
+						<div className="form-error">Пожалуйста, проверьте корректность адреса электронной почты</div>
+						<button type="submit" className="form-next">
+							<i className="material-icons">keyboard_arrow_right</i>
+						</button>
+					</div>
+				</form>
+			</div>
+		)
+	}
+}
