ad160f544e689e615fed5db8240d0cd4e3627def
Alucard163 <32144158+Alucard163@users.noreply.github.com>
2018-07-08 23:29:38 +0400

    Жизненный цикл компонентов

--- lifecycle/search/js/SearchBox.js
+++ lifecycle/search/js/SearchBox.js
@@ -3,17 +3,36 @@ class SearchBox extends React.Component {
   constructor(props) {
     super(props);
     this.state = { fixed: false };
+
   }
+
+  saveSearchBoxTopPosition = (el) => {
+    this.topPosition = el.offsetTop;
+  };
 
   render() {
-    return <SearchBoxView fixed={this.state.fixed} />
+    return <SearchBoxView fixed={this.state.fixed} saveSearchBoxTopPosition={this.saveSearchBoxTopPosition} />
   }
 
-  isFixed() {
-    return undefined;
+  isFixed = (offset) =>  {
+    return offset > this.topPosition;
+  };
+
+  setPosition = (fixed) => {
+    this.setState({
+      fixed: fixed
+    });
+  };
+
+  scrollHandler = () => {
+    this.setPosition(this.isFixed(window.pageYOffset));
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.scrollHandler);
   }
 
-  setPosition() {
-    return undefined;
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.setPosition);
   }
 }
